44cd28feb29ee1d28f1cada6278257a165b6187e
OlehHnyp <lefk6230@gmail.com>
2021-07-26 15:28:46 +0300

    aded src components

---
+++ frontend/src/components/groups/Groups.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Groups extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Groups list</h1>
+            </div>
+        )
+    }
+}
+
+export default Groups
+
